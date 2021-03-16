8c32b7b3219703165ef38f4ad3d8b5c99328c43e
Kerry <kerry@vechtsportonline.nl>
2021-03-16 12:44:09 +0100

    update migration

---
+++ database/migrations/2021_03_16_114040_create_admin_filters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminFiltersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_filters', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('approved');
+            $table->string('status');
+            $table->string('sku');
+            $table->string('product_name');
+            $table->string('category');
+            $table->string('price');
+            $table->string('supplier');
+            $table->string('brand');
+            $table->timestamps();
+            $table->timestamp('last_synced_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_filters');
+    }
+}
