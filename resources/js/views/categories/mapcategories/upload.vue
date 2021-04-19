07d1bf36dc14803203ccb25c96efb5dfd3aa45c5
Kerry <kerry@vechtsportonline.nl>
2021-04-19 15:35:52 +0200

    csv mapping

--- resources/js/views/categories/mapcategories/upload.vue
+++ resources/js/views/categories/mapcategories/upload.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="app-container">
     <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
-    <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
+    <!-- <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
-    </el-table>
+    </el-table> -->
+    <!-- <div class="row" style="margin-top:50px;"> -->
+    <div style="margin-top:50px;">
+      <el-form>
+        <el-form-item :label="user.name">
+          <el-col :span="18">
+            <el-time-picker type="fixed-time" placeholder="Pick a time" />
+          </el-col>
+        </el-form-item>
+      </el-form>
+
+      <el-form :data="tableData" border highlight-current-row style="width: 100%;">
+        <div class="col-12">
+          <el-form-item v-for="item of tableHeader" :key="item" :prop="item" :label="item">
+
+            <el-col :span="18">
+              <el-select style="margin-bottom:10px;">
+                <el-option v-for="items in tableHeader" :key="items" :label="items" :value="items" />
+              </el-select>
+            </el-col>
+          </el-form-item>
+        </div>
+      </el-form>
+    </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -17,9 +41,17 @@ export default {
     return {
       tableData: [],
       tableHeader: [],
+      user: '',
     };
   },
+  mounted: function() {
+    this.getUser();
+  },
   methods: {
+    async getUser() {
+      const data = await this.$store.dispatch('user/getInfo');
+      this.user = data;
+    },
     beforeUpload(file) {
       const isLt1M = file.size / 1024 / 1024 < 1;
 
@@ -36,6 +68,7 @@ export default {
     handleSuccess({ results, header }) {
       this.tableData = results;
       this.tableHeader = header;
+      console.log('table Header: ', this.tableHeader);
     },
   },
 };
