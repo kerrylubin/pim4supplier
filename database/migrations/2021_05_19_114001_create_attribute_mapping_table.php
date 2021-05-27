90a193de9fbf945a19780df1d7bc0d000d1e3ce5
Kerry <kerry@vechtsportonline.nl>
2021-05-27 17:34:24 +0200

    Dropdown add inputs

--- database/migrations/2021_05_19_114001_create_attribute_mapping_table.php
+++ database/migrations/2021_05_19_114001_create_attribute_mapping_table.php
@@ -15,17 +15,17 @@ class CreateAttributeMappingTable extends Migration
     {
 
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->increments('id');
+            $table->unsignedInteger('id');
             $table->string('supplier_name');
             $table->timestamps();
 
-            // $table->foreign('id')
-            // ->references('id')
-            // ->on('users')
-            // ->onDelete('cascade')
-            // ->onUpdate('cascade');
+            $table->foreign('id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
 
-            // $table->primary('id');
+            $table->primary('id');
         });
 
         // Schema::create('users', function (Blueprint $table) {
--- resources/js/views/products/attributes/createattributes/index0.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="Create Attributes" type="success">
-      <router-view />
-    </el-alert>
-  </div>
-</template>
